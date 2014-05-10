ce83c04328f222612dc63aec67d189b0bbe4bb27
dokoresol <do@koresol.com>
2014-05-09 21:36:55 -0700

    after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140421104333) do
+ActiveRecord::Schema.define(:version => 20140509163934) do
+
+  create_table "events", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "messages", :force => true do |t|
+    t.string   "title"
+    t.text     "desc"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "refinery_blog_categories", :force => true do |t|
     t.string   "title"
